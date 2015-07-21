db414d8ceca128caa4d4da780e42d4e417add82d
talyssonoc <talyssonoc@gmail.com>
2015-07-21 19:07:24 -0300

    Update service to upload column

--- app/models/Story.js
+++ app/models/Story.js
@@ -11,6 +11,21 @@ var Story = Waterline.Collection.extend({
     type: 'string',
     project_id: 'integer',
     estimate: 'integer'
+  },
+
+  beforeUpdate(values, next) {
+    if('estimate' in values) {
+      if(values.estimate < 0) {
+        values.state = 'not_estimated';
+      }
+      else if(values.state === 'not_estimated') {
+        values.state = 'icebox';
+      }
+    } else if('state' in values && values.state === 'not_estimated') {
+      values.estimate = -1;
+    }
+
+    next();
   }
 });
 
--- app/services/StoryService.js
+++ app/services/StoryService.js
@@ -6,22 +6,6 @@ class StoryService extends BaseService {
   constructor(options) {
     super(options);
   }
-
-  update(req, resource, params, body, config, callback) {
-
-    if('estimate' in body) {
-      if(body.estimate < 0) {
-        body.state = 'not_estimated';
-      }
-      else if(body.state === 'not_estimated') {
-        body.state = 'icebox';
-      }
-    } else if('state' in body && body.state === 'not_estimated') {
-      body.estimate = -1;
-    }
-
-    super.update(...arguments);
-  }
 }
 
 export default StoryService;
